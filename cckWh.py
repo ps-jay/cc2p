db0bf97c16c03f43e43767b920973d0fd9f30768
Philip Jay <phil@jay.id.au>
2013-05-29 20:42:12 +1000

    Updated

--- cckWh.py
+++ cckWh.py
@@ -1,23 +1,35 @@
 #!/bin/env python26
 
 import calendar
+import datetime
 import sqlite3
+import sys
 import time
 
 values = {
-    'phase1': [],
-    'phase2': [],
-    'phase3': [],
+#    'phase1': [],
+#    'phase2': [],
+#    'phase3': [],
+    'power': [],
 }
 
 kWhs = {
-    'phase1': 0,
-    'phase2': 0,
-    'phase3': 0,
+#    'phase1': 0,
+#    'phase2': 0,
+#    'phase3': 0,
+    'power': 0,
 }
 
-end = calendar.timegm(time.gmtime())
-start = end - 3600
+if len(sys.argv) == 1:
+    print "Last 1 hour"
+    end = calendar.timegm(time.gmtime())
+    start = end - 3600
+else:
+    start = datetime.datetime(2013, 05, int(sys.argv[1]), 15, 0)
+    end = datetime.datetime(2013, 05, int(sys.argv[1]), 20, 0)
+    print "From %s to %s (UTC)" % (start, end,)
+    start = calendar.timegm(start.timetuple())
+    end = calendar.timegm(end.timetuple())
 
 database = sqlite3.connect('/opt/cc/power.sqlite')
 cursor = database.cursor()
@@ -31,7 +43,10 @@ for value in values:
     for i in values[value]:
         kWhs[value] = kWhs[value] + i[1]
     
-    kWhs[value] = float((float(kWhs[value]) / float(len(values[value]))) / 1000)
+    samples = len(values[value])
+    hours = (end - start) / float(3600)
+
+    kWhs[value] = (float(kWhs[value]) / float(samples)) / float(1000)
 
 cursor.close()
 database.close()
@@ -41,5 +56,13 @@ total = 0
 for kWh in kWhs:
     total = total + kWhs[kWh]
 
-print kWhs
-print total
+#print kWhs
+print "From %s samples, over %s:%02d hours & minutes: \n" \
+    " -> the average kWh value is: %.2fkWh \n" \
+    " -> kWh's consumed over the period: %.1fkWhs" % (
+        samples,
+        int(hours),
+        int(hours * 60) % int(60),
+        total,
+        total * hours,
+    )
